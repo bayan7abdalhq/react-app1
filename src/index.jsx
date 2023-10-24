aea410ec4ce45819b72a0e8d37567a6f66a6295d
bayan7abdalhq <s12114374@stu.najah.edu>
2023-10-25 00:00:29 +0300

    reactt

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
