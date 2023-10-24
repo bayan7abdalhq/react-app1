bb06796932afb0457838fa02b37c062872a70bdc
bayan7abdalhq <s12114374@stu.najah.edu>
2023-10-24 23:43:54 +0300

    change

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./component/Footer.jsx";
+import Navbar from "./component/Navbar.jsx";
+import Products from "./component/Products.jsx";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Products/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
